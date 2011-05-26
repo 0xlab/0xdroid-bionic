fc308ba3224e38f0f0c466e0cc56a6b7e8ba9eff
Jim Huang <jserv@0xlab.org>
2011-05-26 11:29:24 +0800

    bionic: Add ARM optimized strcpy()
    Reference results of the experiments on Qualcomm MSM7x25 (528MHz):
    
    [original C code]
                 prc thr   usecs/call      samples   errors cnt/samp
    size
    strcpy_1k      1   1     14.56159           99        0     1000
    1024
    
    [ARM optimized code]
                 prc thr   usecs/call      samples   errors cnt/samp
    size
    strcpy_1k      1   1      3.46653           99        0     1000
    1024

--- libc/Android.mk
+++ libc/Android.mk
@@ -187,7 +187,6 @@ libc_common_src_files := \
 	string/strcat.c \
 	string/strchr.c \
 	string/strcoll.c \
-	string/strcpy.c \
 	string/strcspn.c \
 	string/strdup.c \
 	string/strerror.c \
@@ -354,6 +353,7 @@ libc_common_src_files += \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
 	arch-arm/bionic/strlen.c.arm \
+	arch-arm/bionic/strcpy.S \
 	arch-arm/bionic/syscall.S \
 	string/memmove.c.arm \
 	string/bcopy.c \
@@ -402,6 +402,7 @@ libc_common_src_files += \
 	arch-x86/string/strcmp_wrapper.S \
 	arch-x86/string/strncmp_wrapper.S \
 	arch-x86/string/strlen.S \
+	string/strcpy.c \
 	bionic/pthread-rwlocks.c \
 	bionic/pthread-timers.c \
 	bionic/ptrace.c
@@ -441,6 +442,7 @@ libc_common_src_files += \
 	string/strncmp.c \
 	string/memcmp.c \
 	string/strlen.c \
+	string/strcpy.c \
 	bionic/pthread-rwlocks.c \
 	bionic/pthread-timers.c \
 	bionic/ptrace.c \
